f0eea1c6a74934679142e3a443c02c423993cbe1
陈明鑫 <1934229476@qq.com>
2020-10-13 14:04:09 +0800

    提交到本地仓库

---
+++ git-test/src/com/cmx/FirstTest.java
@@ -0,0 +1,9 @@
+package com.cmx;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("嘿嘿");
+        System.out.println("嘻嘻");
+    }
+}
